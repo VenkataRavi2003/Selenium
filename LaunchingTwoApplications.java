57147de3161d30902377653b43440aae217f649d
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2025-02-13 14:53:04 +0530

    Add files via upload

---
+++ LaunchingTwoApplications.java
@@ -0,0 +1,20 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchingTwoApplications {
+	public static void main(String[] args) throws InterruptedException{
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver wbDriver = new ChromeDriver();
+		wbDriver.get("https://amazon.com");
+		
+		Thread.sleep(4000);
+		
+		wbDriver.get("https://flipkart.com");
+		
+	}
+}

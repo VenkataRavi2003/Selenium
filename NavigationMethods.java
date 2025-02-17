3819dc241bcf308e3ad65c8efa6d040f26a1be13
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2025-02-17 11:19:53 +0530

    Update NavigationMethods.java

--- NavigationMethods.java
+++ NavigationMethods.java
@@ -1,11 +1,7 @@
-package selenium;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class NavigationMethods {
 	public static void main(String[] args)throws InterruptedException {
 		

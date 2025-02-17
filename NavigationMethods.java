89f6c680e31c880e600585595a78bf375dbd4f2a
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2025-02-17 11:19:31 +0530

    Add files via upload

---
+++ NavigationMethods.java
@@ -0,0 +1,49 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class NavigationMethods {
+	public static void main(String[] args)throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver wbDriver = new ChromeDriver();
+		
+		wbDriver.get("https://google.com/");
+		wbDriver.manage().window().maximize();
+		
+		Thread.sleep(3000);
+		
+		//to();
+		wbDriver.navigate().to("https://github.com/login");
+		Thread.sleep(3000);
+		
+		wbDriver.findElement(By.id("login_field")).sendKeys("ramu22@gamil.com");
+		Thread.sleep(3000);
+		
+		wbDriver.findElement(By.id("password")).sendKeys("xyz123");
+		Thread.sleep(3000);
+		
+		wbDriver.findElement(By.name("commit")).click();
+		Thread.sleep(3000);
+		
+		//reload();
+		wbDriver.navigate().refresh();
+		Thread.sleep(3000);
+		
+		//back();
+		wbDriver.navigate().back();
+		
+		//forward();
+		wbDriver.navigate().forward();
+		Thread.sleep(3000);
+		
+		//to
+		wbDriver.navigate().to("https://google.com/");
+		
+		
+	}
+}

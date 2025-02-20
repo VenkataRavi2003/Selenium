20152352ec3dde5e06a62444dbe8df385d66c5bd
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2025-02-20 20:14:09 +0530

    Update ImplicitTimeOut.java

--- ImplicitTimeOut.java
+++ ImplicitTimeOut.java
@@ -1,14 +1,9 @@
-package selenium;
-
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class ImplicitTimeOut {
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();

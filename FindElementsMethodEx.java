d1b85d118287a980a7fb41f104cb1161ab883507
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2025-02-20 21:10:03 +0530

    Update FindElementsMethodEx.java

--- FindElementsMethodEx.java
+++ FindElementsMethodEx.java
@@ -1,9 +1,6 @@
-package selenium;
-
 import java.util.List;
 import java.util.ListIterator;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
